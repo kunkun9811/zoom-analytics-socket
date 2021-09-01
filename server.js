40470a1c9900e398f5ef25ee6fe529094ae0d17b
kunkun9811 <timochen77@gmail.com>
2021-09-01 14:34:29 -0700

    merging

--- server.js
+++ server.js
@@ -3,17 +3,30 @@ const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:3001"],
-  },
-});
+const io = new Server(server);
+const cors = require("cors");
+
 const chat = require("./analytics");
 const port = 4001;
+
+var whitelist = ["http://localhost:4001", "http://www.zoomdemo.aankh.co/overlay", "https://www.zoomdemo.aankh.co/overlay", "http://www.zoomdemo.aankh.co", "https://www.zoomdemo.aankh.co/overlay"];
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+app.options("*", cors(corsOptions)); // include before other routes
 
 app.get("/", (req, res) => {
   res.send("<h1>Socket server :)</h1>");
 });
+
+// app.options();
 
 chat(io);
 
